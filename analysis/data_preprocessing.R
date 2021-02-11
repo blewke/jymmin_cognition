207fef128331a8544c4b268dd2045a0f5ed6b94c
blewke <lewkebritta@gmail.com>
2021-02-11 09:44:07 +0100

    changes in the data preprocesssing stage

--- analysis/data_preprocessing.R
+++ analysis/data_preprocessing.R
@@ -28,6 +28,15 @@ data_preprocessing = function (datafilename){
   all_data$TimePoint = NA
   all_data$TimePoint = as.numeric(dmy_hms(paste(all_data$Date,all_data$Time)))
   
+  
+  all_data$nCorrect = NA
+  all_data$nCorrect = as.integer(all_data$Accuracy*all_data$ClocksInSet*0.01)
+  
+  all_data$LevelType = NA
+  all_data$LevelType = all_data$Level%%5
+  
+  
+  
   return(all_data)
 
 }
@@ -48,8 +57,11 @@ all_data = dataframe
 
 
 #random bit fixed list of group membership
-A = c(4055, 4059, 4063, 4064, 4068, 4070, 4072, 4073, 4074, 4077, 4082, 4083)
-B = c(4052, 4056, 4058, 4062, 4065, 4066, 4067, 4069, 4071, 4076, 4078, 4080)
+#A = c(4055, 4059, 4063, 4064, 4068, 4070, 4072, 4073, 4074, 4077, 4082, 4083)
+#B = c(4052, 4056, 4058, 4062, 4065, 4066, 4067, 4069, 4071, 4076, 4078, 4080)
+
+A = 1:12
+B = 13:24
 
 all_data$Group = NA
 all_data$Group[all_data$SubjectCode %in% A] = 'A'
@@ -64,24 +76,70 @@ end_day = as.numeric(dmy_hms(paste('01.08.2018', '23:59:59')))
 
 all_data$Period = NA
 all_data$Period[all_data$TimePoint  >= test_start_day & all_data$TimePoint <= end_first_period]= 1
-all_data$Period[all_data$TimePoint  >= begin_second_period & all_data$tTimePoint <= end_day]= 2
+all_data$Period[all_data$TimePoint  >= begin_second_period & all_data$TimePoint <= end_day]= 2
 
-all_data$Assignment = NA
-all_data$Assignment[!is.na(all_data$Group) & all_data$Group=='A'& all_data$Period == 1] = 'Yes'
-all_data$Assignment[!is.na(all_data$Group) & all_data$Group=='B'& all_data$Period == 2] = 'Yes'
-all_data$Assignment[!is.na(all_data$Group) & all_data$Group=='A'& all_data$Period == 2] = 'No'
-all_data$Assignment[!is.na(all_data$Group) & all_data$Group=='B'& all_data$Period == 1] = 'No'
+#all_data$Period[all_data$TimePoint  >= 1532044800 & all_data$tTimePoint  <= 1533935260]= 2
+
+all_data$Jymmin = NA
+all_data$Jymmin[!is.na(all_data$Group) & all_data$Group=='A'& all_data$Period == 1] = 'Yes'
+all_data$Jymmin[!is.na(all_data$Group) & all_data$Group=='B'& all_data$Period == 2] = 'Yes'
+all_data$Jymmin[!is.na(all_data$Group) & all_data$Group=='A'& all_data$Period == 2] = 'No'
+all_data$Jymmin[!is.na(all_data$Group) & all_data$Group=='B'& all_data$Period == 1] = 'No'
 
 
 
-d = all_data[!is.na(all_data$Group), ]
+#d = all_data[!is.na(all_data$Group), ]
 #d = d['Exercise'== d$Stage, ]
 
-d$Period[is.na(d$Period)]=0
-d$Assignment[is.na(d$Assignment)]='.'
+#d$Period[is.na(d$Period)]=0
+#d$Assignment[is.na(d$Assignment)]='.'
 
-return(d)
+#return(d)
+
+
+return(all_data)
 
 }
+
+
+
+
+
+
+remove_empty_rows = function (df){
+  
+  return(df[df$Type =='',])
+}
+
+
+add_nTrialLevel = function(df){
+  
+  df$nTrialLevel = NA
+  
+  df = df[order(df$TimePoint),]
+  
+  
+  for(s in unique(df$SubjectCode)){
+    for (l in unique(df$Level)){
+      
+      rowindices = which(df$SubjectCode==s & df$Level == l & !is.na(df$ClocksInSet))
+      #sldat = sldat[order(sldat$transformed_time24),]
+      for  (ir in 1:length(rowindices))
+      {
+        if (ir == 1)  {
+          df$nTrialLevel[rowindices[ir]] = 0
+        }else{
+          df$nTrialLevel[rowindices[ir]] = df$nTrialLevel[rowindices[ir-1]] + df$ClocksInSet[rowindices[ir-1]]
+        }
+      }
+    }
+  }
+  
+  return(df) 
+  
+  
+  
+}
+
 
 
