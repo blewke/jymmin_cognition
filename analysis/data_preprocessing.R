7547d73c4be7fe7f3e2224a79f1fb6406b55c2ae
blewke <lewkebritta@gmail.com>
2021-02-17 19:31:57 +0100

    made some adjustments for the non-blinded data file

--- analysis/data_preprocessing.R
+++ analysis/data_preprocessing.R
@@ -8,7 +8,7 @@
 #' @return A data frame with the loaded data.
 
 
-data_preprocessing = function (datafilename){
+data_preprocessing = function (datafilename, dateformat = 'dmy'){
   require(lubridate)
   all_data = read.csv(datafilename,
                       stringsAsFactors = FALSE)
@@ -26,8 +26,12 @@ data_preprocessing = function (datafilename){
   
   
   all_data$TimePoint = NA
+  if (dateformat == 'mdy'){
+    all_data$TimePoint = as.numeric(mdy_hms(paste(all_data$Date,all_data$Time)))
+  }else{
+    
   all_data$TimePoint = as.numeric(dmy_hms(paste(all_data$Date,all_data$Time)))
-  
+  }
   
   all_data$nCorrect = NA
   all_data$nCorrect = as.integer(all_data$Accuracy*all_data$ClocksInSet*0.01)
@@ -100,6 +104,65 @@ all_data$Jymmin[!is.na(all_data$Group) & all_data$Group=='B'& all_data$Period ==
 return(all_data)
 
 }
+
+
+
+
+real_assignment = function(dataframe){
+  require (lubridate)
+  
+  all_data = dataframe
+  
+  
+  #random bit fixed list of group membership
+  #A = c(4055, 4059, 4063, 4064, 4068, 4070, 4072, 4073, 4074, 4077, 4082, 4083)
+  #B = c(4052, 4056, 4058, 4062, 4065, 4066, 4067, 4069, 4071, 4076, 4078, 4080)
+  
+  A = c('A','B','C','D')
+  B = c('E','F','G','H')
+  
+  all_data$Group = NA
+  all_data$Group[substring(all_data$SubjectCode,1,1) %in% A] = 'A'
+  all_data$Group[substring(all_data$SubjectCode,1,1) %in% B] = 'B'
+  
+  
+  # fixed dates
+  test_start_day = as.numeric(dmy_hms(paste('6.07.2018', '0:00:00')))
+  end_first_period = as.numeric(dmy_hms(paste('18.07.2018', '23:59:59')))
+  begin_second_period = as.numeric(dmy_hms(paste('20.07.2018', '0:00:00')))
+  end_day = as.numeric(dmy_hms(paste('01.08.2018', '23:59:59')))
+  
+  all_data$Period = NA
+  all_data$Period[all_data$TimePoint  >= test_start_day & all_data$TimePoint <= end_first_period]= 1
+  all_data$Period[all_data$TimePoint  >= begin_second_period & all_data$TimePoint <= end_day]= 2
+  
+  #all_data$Period[all_data$TimePoint  >= 1532044800 & all_data$tTimePoint  <= 1533935260]= 2
+  
+  all_data$Jymmin = NA
+  all_data$Jymmin[!is.na(all_data$Group) & all_data$Group=='A'& all_data$Period == 1] = 'Yes'
+  all_data$Jymmin[!is.na(all_data$Group) & all_data$Group=='B'& all_data$Period == 2] = 'Yes'
+  all_data$Jymmin[!is.na(all_data$Group) & all_data$Group=='A'& all_data$Period == 2] = 'No'
+  all_data$Jymmin[!is.na(all_data$Group) & all_data$Group=='B'& all_data$Period == 1] = 'No'
+  
+  
+  
+  #d = all_data[!is.na(all_data$Group), ]
+  #d = d['Exercise'== d$Stage, ]
+  
+  #d$Period[is.na(d$Period)]=0
+  #d$Assignment[is.na(d$Assignment)]='.'
+  
+  #return(d)
+  
+  
+  return(all_data)
+  
+}
+
+
+
+
+
 
 
 
