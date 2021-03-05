9083df4059f67adc81393d92c952b50e3cb45f79
blewke <lewkebritta@gmail.com>
2021-03-05 15:37:56 +0100

    made priors for DurationInSeconds narrower and switched from student_t to normal

--- analysis/priors.R
+++ analysis/priors.R
@@ -3,23 +3,33 @@
 
 
 priors_duration = #prior(student_t(3,2,8), class = Intercept, resp = 'DurationInSeconds'),
-   #c(prior(student_t(3,2,2.5), class = Intercept, resp = 'DurationInSeconds'), ## new!
-     c(prior(student_t(3,1,1), class = Intercept, resp = 'DurationInSeconds'),
+   c(
+    #prior(student_t(3,2,2.5), class = Intercept, resp = 'DurationInSeconds'), ## new!
+     #prior(student_t(3,1,1), class = Intercept, resp = 'DurationInSeconds'),
+     prior(normal(1.5,1.75), class = Intercept, resp = 'DurationInSeconds'),
      
+     #prior(student_t(3,0,1.5), class = sd, group = 'Level', resp = 'DurationInSeconds'), # new!!
+     #prior(student_t(3,0,1.5), class = sd, group = 'SubjectCode', resp = 'DurationInSeconds'),
+     prior(normal(0,0.5), class = sd, group = 'Level', resp = 'DurationInSeconds'), 
+     prior(normal(0,0.5), class = sd, group = 'SubjectCode', resp = 'DurationInSeconds'),
      
-     #prior(student_t(3,0,2), class = sd, group = 'Level', resp = 'DurationInSeconds'),
-     prior(student_t(3,0,1.5), class = sd, group = 'Level', resp = 'DurationInSeconds'), # new!!
-     prior(student_t(3,0,1.5), class = sd, group = 'SubjectCode', resp = 'DurationInSeconds'),
-     prior(student_t(3,0,5), class = sigma, resp = 'DurationInSeconds'),
+     #prior(student_t(3,0,5), class = sigma, resp = 'DurationInSeconds'),
+     prior(normal(0,1), class = sigma, resp = 'DurationInSeconds'),
      
      ##!!prior(normal(1,2), class = shift, resp = 'DurationInSeconds'),
      #prior(student_t(3,2,3), class = shift, resp = 'DurationInSeconds'),
      prior(gamma(2,1), class = shift, resp = 'DurationInSeconds'),
      
-     prior(student_t(3, -0.5, 1.5), class = b, coef = 'nTrialLevelScale', resp = 'DurationInSeconds'),
-     prior(student_t(3,0,0.5), class = sd, coef = 'nTrialLevelScale', group = 'SubjectCode', resp = 'DurationInSeconds'),
-     prior(student_t(3,0,0.5), class = sd, coef = 'nTrialLevelScale', group = 'Level', resp = 'DurationInSeconds')
-)
+     #prior(student_t(3, -0.5, 1.5), class = b, coef = 'nTrialLevelScale', resp = 'DurationInSeconds'),
+     prior(normal(-0.25, 0.5), class = b, coef = 'nTrialLevelScale', resp = 'DurationInSeconds'),
+     
+     #(student_t(3,0,0.5), class = sd, coef = 'nTrialLevelScale', group = 'SubjectCode', resp = 'DurationInSeconds'),
+     #prior(student_t(3,0,0.5), class = sd, coef = 'nTrialLevelScale', group = 'Level', resp = 'DurationInSeconds')
+
+     prior(normal(0,0.25), class = sd, coef = 'nTrialLevelScale', group = 'SubjectCode', resp = 'DurationInSeconds'),
+     prior(normal(0,0.25), class = sd, coef = 'nTrialLevelScale', group = 'Level', resp = 'DurationInSeconds')
+     
+     )
 
 
 priors_accuracy = c(prior(student_t(3,0,2.5), class = Intercept, resp = 'nCorrect'),
