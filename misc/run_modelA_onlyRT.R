1c7c236414e681b5d1581f21703d5b783e872e3c
blewke <lewkebritta@gmail.com>
2021-03-05 16:27:07 +0100

    included prior without resp argument

--- misc/run_modelA_onlyRT.R
+++ misc/run_modelA_onlyRT.R
@@ -36,6 +36,41 @@ rm(study_data)
 rm(rdata)
 
 
+priors_duration = #prior(student_t(3,2,8), class = Intercept, resp = 'DurationInSeconds'),
+  c(
+    #prior(student_t(3,2,2.5), class = Intercept, resp = 'DurationInSeconds'), ## new!
+    #prior(student_t(3,1,1), class = Intercept, resp = 'DurationInSeconds'),
+    prior(normal(1.5,1.75), class = Intercept),# resp = 'DurationInSeconds'),
+    
+    #prior(student_t(3,0,1.5), class = sd, group = 'Level', resp = 'DurationInSeconds'), # new!!
+    #prior(student_t(3,0,1.5), class = sd, group = 'SubjectCode', resp = 'DurationInSeconds'),
+    prior(normal(0,0.5), class = sd, group = 'Level'),# resp = 'DurationInSeconds'), 
+    prior(normal(0,0.5), class = sd, group = 'SubjectCode'),# resp = 'DurationInSeconds'),
+    
+    #prior(student_t(3,0,5), class = sigma, resp = 'DurationInSeconds'),
+    prior(normal(0,1), class = sigma),# resp = 'DurationInSeconds'),
+    
+    ##!!prior(normal(1,2), class = shift, resp = 'DurationInSeconds'),
+    #prior(student_t(3,2,3), class = shift, resp = 'DurationInSeconds'),
+    prior(gamma(2,1), class = shift),# resp = 'DurationInSeconds'),
+    
+    #prior(student_t(3, -0.5, 1.5), class = b, coef = 'nTrialLevelScale', resp = 'DurationInSeconds'),
+    prior(normal(-0.25, 0.5), class = b, coef = 'nTrialScale'),# resp = 'DurationInSeconds'),
+    
+    #(student_t(3,0,0.5), class = sd, coef = 'nTrialLevelScale', group = 'SubjectCode', resp = 'DurationInSeconds'),
+    #prior(student_t(3,0,0.5), class = sd, coef = 'nTrialLevelScale', group = 'Level', resp = 'DurationInSeconds')
+    
+    prior(normal(0,0.25), class = sd, coef = 'nTrialScale', group = 'SubjectCode'),# resp = 'DurationInSeconds'),
+    prior(normal(0,0.25), class = sd, coef = 'nTrialScale', group = 'Level')#, resp = 'DurationInSeconds')
+    
+  )
+
+
+
+
+
+
+
 priorModelA_rt = priors_duration
 
 ModelA =  brm (
@@ -52,7 +87,7 @@ ModelA =  brm (
   cores = 6,
   chains = 6,
   warmup = 100,
-  iter = 100,
+  iter = 200,
   seed = 4,
   file = '../results/ModelA_rt',
   sample_file = '../results/ModelArtchaindata',
